bd024dc3084ba39af5d77dfd69a4e47ca35191ed
Your Name <you@example.com>
2024-11-04 18:52:30 +0800

    intial commit

---
+++ userAge.js
@@ -0,0 +1,15 @@
+let age = "25";
+
+if (age < 5){
+    console.log("You are a toddler.");
+} else if (age <= 12){
+    console.log("You are a child.");
+}else if (age <= 19){
+    console.log("You are a Teenager.");
+}else if (age <= 35){
+    console.log("You are a Young Adult");
+}else if (age <= 60){
+    console.log("You are a Middle-aged");
+}else {
+    console.log("You are a Senior.")
+}
